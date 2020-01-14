d1718eb3500b27028b0a82dc883cf3b59d67d3f9
Mirko Cukich <digitalmirko@aol.com>
2020-01-14 13:49:59 -0600

    Code added to bottom toolbar buttons for actions required

--- Web Browser/ViewController.m
+++ Web Browser/ViewController.m
@@ -23,14 +23,26 @@
 
 
 - (IBAction)stopBtn:(id)sender {
+    
+    [self.webView stopLoading];
 }
 
 - (IBAction)refreshBtn:(id)sender {
+    
+    [self.webView reload];
 }
 
 - (IBAction)backBtn:(id)sender {
+    // checks if can to go back
+    if([self.webView canGoBack]){
+        [self.webView goBack];
+    }
 }
 
 - (IBAction)forwardBtn:(id)sender {
+    // checks if can go forward
+    if([self.webView goForward]){
+        [self.webView goForward];
+    }
 }
 @end
